bf2ddb4b351d7a632db214e7f20142fda9619f0d
Dominique Sakrisson <domsakrisson@gmail.com>
2019-05-01 15:13:28 -0700

    Fixed issue with the ninja list not displaying by accessing the objects infomation but rather the object tag itself

--- app/app.js
+++ app/app.js
@@ -1,40 +1,43 @@
 var myNinjaApp = angular.module('myNinjaApp',[]);
 // [] above are where modules belong
-
 //How does angular know what will be controlling the application?
 //A: set the ng-app eqaul to the name of the module like above
 //A: looks like ng-app="myNinjaApp" (in the html file)
-
-
 //all of the controllers, configs, root menus, will be contained in this module
-
-
 //best practice to name the dependencies and required to include them in function call
 myNinjaApp.controller('NinjaController', ['$scope', function($scope){
 
-  $scope.message = "Hey";
+  $scope.removeNinja = function(ninja){
+    var removedNinja = $scope.ninjas.indexOf(ninja);
+    $scope.ninjas.splice(removedNinja, 1);
+  }
+
 
   $scope.ninjas = [
   {
     name: "Yoshi",
     belt: "green",
-    rate: 50
+    rate: 50,
+    available: true
   },
   {
     name: "crystal",
     belt: "green",
-    rate: 30
+    rate: 30,
+    available: true
   },
   {
     name: "ryu",
     belt: "orange",
-    rate: 100
-
+    rate: 100,
+    available: true
   },
   {
     name: "shawn",
     belt: "black",
-    rate: 1000
+    rate: 1000,
+    available: false
   }
 ];
+
 }]);
