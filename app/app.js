5cd83bd5d2e0104e142cfa651803e159ff621117
Dominique Sakrisson <domsakrisson@gmail.com>
2019-05-01 22:04:26 -0700

    fixed bug where the nav buttons didnt show ninja directory view. Enabled  allowing us to store the ninjas array data in a json file contain the object attributes.

--- app/app.js
+++ app/app.js
@@ -1,11 +1,28 @@
-var myNinjaApp = angular.module('myNinjaApp',[]);
+var myNinjaApp = angular.module('myNinjaApp',['ngRoute']);
 // [] above are where modules belong
 //How does angular know what will be controlling the application?
 //A: set the ng-app eqaul to the name of the module like above
 //A: looks like ng-app="myNinjaApp" (in the html file)
 //all of the controllers, configs, root menus, will be contained in this module
 //best practice to name the dependencies and required to include them in function call
-myNinjaApp.controller('NinjaController', ['$scope', function($scope){
+
+
+//config method needs to be first so that the module can manage to load all dependencies first
+
+myNinjaApp.config(['$routeProvider', function($routeProvider){
+  $routeProvider
+    .when('/home', {
+      templateUrl: 'views/home.html',
+    })
+    .when('/directory', {
+      templateUrl: 'views/directory.html',
+      controller: 'NinjaController'
+    }).otherwise({
+      redirectTo: '/home'
+    });
+}]);
+
+myNinjaApp.controller('NinjaController', ['$scope', '$http', function($scope, $http){
 
   $scope.removeNinja = function(ninja){
     var removedNinja = $scope.ninjas.indexOf(ninja);
@@ -19,37 +36,17 @@ myNinjaApp.controller('NinjaController', ['$scope', function($scope){
       rate: parseInt($scope.newninja.rate),
       available: true
     });
+
     $scope.newninja.name= "";
     $scope.newninja.belt= "";
     $scope.newninja.rate= "";
+
   };
 
+  $http.get('data/ninjas.json').then(function(response){
+    $scope.ninjas = response.data;
+  });
 
-  $scope.ninjas = [
-  {
-    name: "Yoshi",
-    belt: "green",
-    rate: 50,
-    available: true
-  },
-  {
-    name: "crystal",
-    belt: "green",
-    rate: 30,
-    available: true
-  },
-  {
-    name: "ryu",
-    belt: "orange",
-    rate: 100,
-    available: true
-  },
-  {
-    name: "shawn",
-    belt: "black",
-    rate: 1000,
-    available: false
-  }
-];
+//console.log(angular.toJson($scope.ninjas));
 
 }]);
